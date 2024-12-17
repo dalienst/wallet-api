aae85e8dfc70f5d14312f1a4f5726e89829afd17
dalienst <oduordalienst@gmail.com>
2024-12-17 11:08:43 +0300

    add verify email serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,6 +9,7 @@ from accounts.validators import (
     validate_password_lowercase,
     validate_password_symbol,
 )
+from verification.models import VerificationCode
 
 User = get_user_model()
 
@@ -61,3 +62,34 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class VerifyCodeSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    code = serializers.CharField()
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        code = attrs.get("code")
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("Account with this email does not exist!")
+
+        try:
+            verification = user.verification_codes.get(
+                code=code, purpose="email_verification", used=False
+            )
+        except VerificationCode.DoesNotExist:
+            raise serializers.ValidationError("Invalid or expired verification code!")
+
+        if not verification.is_valid():
+            raise serializers.ValidationError(
+                "The code has expired or already been used!"
+            )
+
+        attrs["user"] = user
+        attrs["verification"] = verification
+
+        return attrs
--- accounts/utils.py
+++ accounts/utils.py
@@ -1,6 +1,10 @@
 import string
 import secrets
 
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from wallet.settings import DOMAIN, EMAIL_USER
+
 
 def generate_slug():
     characters = string.ascii_letters + string.digits
@@ -14,14 +18,22 @@ def generate_reference():
     return random_string.upper()
 
 
+def send_verification_email(user, verification_code):
+    """
+    A function to send a verification email
+    """
+    email_body = render_to_string(
+        "account_verification.html",
+        {
+            "user": user,
+            "verification_code": verification_code,
+        },
+    )
 
-def generate_verification_code():
-    characters = string.ascii_letters + string.digits
-    random_string = "".join(secrets.choice(characters) for _ in range(6))
-    return random_string
-
-
-def generate_password_reset_code():
-    characters = string.ascii_letters + string.digits
-    random_string = "".join(secrets.choice(characters) for _ in range(6))
-    return random_string
+    send_mail(
+        "Verify your account",
+        email_body,
+        EMAIL_USER,
+        [user.email],
+        fail_silently=False,
+    )
