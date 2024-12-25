3b62476038ab0b9c6b5d921a5ec684f1dc4b6d7b
dalienst <oduordalienst@gmail.com>
2024-12-25 10:55:30 +0300

    added projects model

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from accounts.abstracts import UniversalIdModel, ReferenceSlugModel, TimeStampedModel
+
+User = get_user_model()
+
+
+class Project(UniversalIdModel, ReferenceSlugModel, TimeStampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="projects")
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
