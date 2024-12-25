1261ea0ac2735acaa840fcbe4ef44a71f03827ec
dalienst <oduordalienst@gmail.com>
2024-12-25 15:38:29 +0300

    linked apps

--- projects/serializers.py
+++ projects/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from projects.models import Project
+from tasks.serializers import TaskSerializer
 
 
 class ProjectSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source="user.email", read_only=True)
+    tasks = TaskSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
@@ -12,6 +14,7 @@ class ProjectSerializer(serializers.ModelSerializer):
             "id",
             "user",
             "title",
+            "tasks",
             "created_at",
             "updated_at",
             "reference",
