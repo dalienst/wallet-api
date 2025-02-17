2899f9c639f21d5155ed834c7bc5a2be91fdf1f2
dalienst <oduordalienst@gmail.com>
2025-02-17 08:15:12 +0300

    add views

--- wallets/views.py
+++ wallets/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from wallets.models import Wallet
+from wallets.serializers import WalletSerializer
+
+class WalletListCreateView(generics.ListCreateAPIView):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletSerializer
+    permission_classes = [IsAuthenticated,]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
