5d12b7c01d65b1bd5c8f506832cda24a7ff6674f
dalienst <oduordalienst@gmail.com>
2025-02-19 13:44:41 +0300

    add serializers

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,3 +1,24 @@
 from rest_framework import serializers
 
 from transactions.models import Transaction
+from wallets.models import Wallet
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    wallet = serializers.SlugRelatedField(
+        slug_field="reference",
+        queryset=Wallet.objects.all(),
+    )
+
+    class Meta:
+        model = Transaction
+        fields = (
+            "id",
+            "wallet",
+            "amount",
+            "status",
+            "reference",
+            "slug",
+            "created_at",
+            "updated_at",
+        )
